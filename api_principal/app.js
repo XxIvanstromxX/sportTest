2219bc389955e930ae7749bd70b507d1997d375b
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-24 23:28:34 -0600

    Agregar archivo app.js con configuración básica de Express y conexión a MongoDB

---
+++ api_principal/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const morgan = require("morgan");
+const contectDB = require("./config/db");
+require("dotenv").config();
+const { PORT } = process.env;
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Connect to MongoDB
+contectDB();
+
+app.use("/api/auth", require("./routes/auth"));
+//app.use("/api/users", require("./routes/users"));
+//app.use("/api/admin", require("./routes/admin"));
+//app.use("/api/products", require("./routes/products"));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
