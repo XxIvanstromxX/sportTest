ceebc96fdbe862572bbaf7812af4ac734fa6f22c
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-24 23:28:53 -0600

    Agregar modelo de producto con esquema y validaciones

---
+++ api_principal/models/products.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  category: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+    min: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
