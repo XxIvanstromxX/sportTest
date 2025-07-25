b27e69ac7ceeab42f02b18d495f2ce347d861346
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-25 15:45:40 -0600

    Agregar validaciones y preferencias de usuario en el modelo de usuario

--- api_principal/models/users.js
+++ api_principal/models/users.js
@@ -1,6 +1,27 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const VALID_SIZES = ["XS", "S", "M", "L", "XL", "XXL"];
+const VALID_COLORS = [
+  "negro",
+  "blanco",
+  "azul",
+  "rojo",
+  "verde",
+  "gris",
+  "rosa",
+  "amarillo",
+];
+const VALID_BRANDS = [
+  "Nike",
+  "Adidas",
+  "Puma",
+  "Under Armour",
+  "Reebok",
+  "New Balance",
+];
+const VALID_ROLES = ["user", "admin"];
+
 const userSchema = new Schema({
   name: {
     type: String,
@@ -22,8 +43,36 @@ const userSchema = new Schema({
   },
   role: {
     type: String,
-    enum: ["user", "admin"],
+    enum: VALID_ROLES,
     default: "user",
+  },
+  preferences: {
+    default: {},
+    favoriteSizes: {
+      type: [String],
+      enum: VALID_SIZES,
+      default: [],
+    },
+    favoriteColors: {
+      type: [String],
+      enum: VALID_COLORS,
+      default: [],
+    },
+    favoriteBrands: {
+      type: [String],
+      enum: VALID_BRANDS,
+      default: [],
+    },
+  },
+  chatHistory: {
+    type: [
+      {
+        preguntaUsuario: { type: String, required: true },
+        respuestaChatbot: { type: String, required: true },
+        fecha: { type: Date, default: Date.now }, // Fecha de la interacción
+      },
+    ],
+    default: [], // Valor por defecto: un array vacío
   },
   createdAt: {
     type: Date,
