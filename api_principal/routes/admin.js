da5c46f8ce0774e9f5a63b71d06c14cb4626ead5
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-31 23:05:53 -0600

    Agregar ruta para crear productos en el controlador de administración

--- api_principal/routes/admin.js
+++ api_principal/routes/admin.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/authMiddleware");
 const { authRoleMiddleware } = require("../middlewares/roleMiddleware");
-const { getUsers } = require("../controllers/adminController");
+const { getUsers, createProduct } = require("../controllers/adminController");
 
 router.get("/users", protect, authRoleMiddleware("admin"), getUsers);
 
+router.post(
+  "/create-product",
+  protect,
+  authRoleMiddleware("admin"),
+  createProduct
+);
 module.exports = router;
