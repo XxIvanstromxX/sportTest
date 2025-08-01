5e8c7435753581f5c42224493552daefc6f4fcdc
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-31 23:05:34 -0600

    Agregar controlador para obtener todos los productos y por ID

---
+++ api_principal/controllers/productController.js
@@ -0,0 +1,28 @@
+const Product = require("../models/productsModel");
+
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getProductById = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const product = await Product.findById(id);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+};
