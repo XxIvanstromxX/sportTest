19162d5caa8f29801e481971bc1ec5585c7ddb37
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-07-31 23:05:30 -0600

    Agregar controlador para crear productos y corregir mensaje en getUsers

--- api_principal/controllers/adminController.js
+++ api_principal/controllers/adminController.js
@@ -1,3 +1,4 @@
+const Product = require("../models/productsModel");
 const User = require("../models/users");
 
 const getUsers = async (req, res) => {
@@ -8,10 +9,60 @@ const getUsers = async (req, res) => {
       return res.status(404).json({ message: "No users found" });
     }
 
-    return res.status(200).json({ messsage: "Usuarios encontrados", users });
+    return res.status(200).json({ message: "Usuarios encontrados", users });
   } catch (err) {}
+};
+
+const createProduct = async (req, res) => {
+  const { name, description, price, category, brand, size, color, stock } =
+    req.body;
+  console.log("Received product data:", req.body);
+  if (
+    !name ||
+    !description ||
+    !price ||
+    !category ||
+    !brand ||
+    !size ||
+    !color ||
+    stock === undefined
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  try {
+    console.log("Creating product with data:", {
+      name,
+      description,
+      price,
+      category,
+      brand,
+      size,
+      color,
+      stock,
+    });
+    const newProduct = new Product({
+      name,
+      description,
+      price,
+      category,
+      brand,
+      size,
+      color,
+      stock,
+    });
+
+    await newProduct.save();
+    return res
+      .status(201)
+      .json({ message: "Product created successfully", product: newProduct });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Server error", error: error.message });
+  }
 };
 
 module.exports = {
   getUsers,
+  createProduct,
 };
