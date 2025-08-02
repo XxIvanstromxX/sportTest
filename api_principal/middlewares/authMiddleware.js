598e2f051e7028baf905324122c653983a4525f8
XxIvanstromxX <axel-ivan24@hotmail.com>
2025-08-02 17:54:42 -0600

    Refactor middleware de autenticación para omitir solicitudes de chatbot

--- api_principal/middlewares/authMiddleware.js
+++ api_principal/middlewares/authMiddleware.js
@@ -3,16 +3,25 @@ const User = require("../models/users");
 require("dotenv").config();
 
 const protect = async (req, res, next) => {
-  if (
-    !req.headers.authorization ||
-    !req.headers.authorization.startsWith("Bearer")
-  ) {
+  //Check if the request is from a chatbot, validate de X-API-Key header
+  // Esto funciona para evitar que el middleware de autenticación se aplique a las peticiones del chatbot
+  // Además funciona para fusionar el microservicio del chatbot con la API principal
+  const apiKey = req.headers["x-api-key"];
+  if (apiKey && apiKey === process.env.CHATBOT_API_KEY) {
+    console.log("Chatbot request detected, skipping authentication");
+    return next();
+  }
+
+  //Check if the request has a valid JWT token
+  const authHeader = req.headers["authorization"];
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res
       .status(401)
       .json({ message: "No token provided, authorization denied" });
   }
+
   try {
-    const token = req.headers.authorization.split(" ")[1];
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = await User.findById(decoded.userId).select("-password");
     console.log("User found:", req.user);
